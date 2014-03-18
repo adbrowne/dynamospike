da06f15aca408177fa1154a6ac1bf1b6c35d124e
Andrew Browne <brownie@brownie.com.au>
2014-03-18 23:03:08 +1100

    Removed old create table string

--- HttpConduitSample.hs
+++ HttpConduitSample.hs
@@ -133,8 +133,6 @@ dynamoReq operation sink body = do
     
 main :: IO ()
 main = do
-    let createTableString = "{ \"AttributeDefinitions\": [ { \"AttributeName\": \"ForumName\", \"AttributeType\": \"S\" }, { \"AttributeName\": \"Subject\", \"AttributeType\": \"S\" }, { \"AttributeName\": \"LastPostDateTime\", \"AttributeType\": \"S\" } ], \"TableName\": \"Thread2\", \"KeySchema\": [ { \"AttributeName\": \"ForumName\", \"KeyType\": \"HASH\" }, { \"AttributeName\": \"Subject\", \"KeyType\": \"RANGE\" } ], \"LocalSecondaryIndexes\": [ { \"IndexName\": \"LastPostIndex\", \"KeySchema\": [ { \"AttributeName\": \"ForumName\", \"KeyType\": \"HASH\" }, { \"AttributeName\": \"LastPostDateTime\", \"KeyType\": \"RANGE\" } ], \"Projection\": { \"ProjectionType\": \"KEYS_ONLY\" } } ], \"ProvisionedThroughput\": { \"ReadCapacityUnits\": 5, \"WriteCapacityUnits\": 5 } }"
-    putStrLn $ createTableString 
     let myObj = CreateReq { 
                     attributeDefinitions = [ 
                         AttributeDef { attributeName = "ForumName", attributeType = "S" },
